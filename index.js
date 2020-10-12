abd4e539648f5d020cd87bd1e98eb685e250ac88
Mian Shah <mshah@amctechnology.com>
2020-10-12 13:12:42 -0400

    working fetch requests from client app

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const Yelp = require('./yelp');
 const API_KEY = "FLMeJY8fzwqhc33W3NgcwHB3ivXaFmw3ImKNbySrooXJp7yC_nuytgj56g9zzHjQUcUzVP2dxqt-gTxdEYCIahSGkoU5WM9lOAe3lnwHVXumJQ7q6g0-ojYDDARNX3Yx"
 const yelp = new Yelp({ apiKey: API_KEY  })
 
+
+
 var app = express();
-let settings = (res) => {res.header(('Access-Control-Allow-Origin', '*'),("Access-Control-Allow-Headers","Content-Type,Authorization"),("Access-Control-Allow-Methods","GET")
-);}
+
 
 app.use(express.static('public'));
-app.use(settings);
 
 
 
@@ -26,6 +26,7 @@ app.get('/api/search', (req,res) => {
         error: 'No Phone Number'
       })
     }
+    // res.setHeader("Access-Control-Allow-Origin", "*");
    
     yelp.search({phone})
     .then(data => {
