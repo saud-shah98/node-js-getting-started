369cff6c58cc2179cf871e5ec341f760539fb2ab
Mian Shah <mshah@amctechnology.com>
2020-10-12 12:45:34 -0400

    proper headers

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ var fs = require('fs')
 require('dotenv').config();
 var path = require('path');
 var express = require('express');
+var cors_proxy = require('cors-anywhere');
+
 const Yelp = require('./yelp');
 const API_KEY = "FLMeJY8fzwqhc33W3NgcwHB3ivXaFmw3ImKNbySrooXJp7yC_nuytgj56g9zzHjQUcUzVP2dxqt-gTxdEYCIahSGkoU5WM9lOAe3lnwHVXumJQ7q6g0-ojYDDARNX3Yx"
 const yelp = new Yelp({ apiKey: API_KEY  })
 
 var app = express();
-let settings = (res) => {res.setHeader('Access-Control-Allow-Origin', '*');}
+let settings = (res) => {res.header(('Access-Control-Allow-Origin', '*'),("Access-Control-Allow-Headers","Content-Type,Authorization"),("Access-Control-Allow-Methods","GET")
+);}
 
 app.use(express.static('public'));
 app.use(settings);
